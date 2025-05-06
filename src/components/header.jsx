51f523638d8804b1e07b696988b8304eb888c7e6
ArtOleinikov <artem.oleinikov.web@gmail.com>
2025-05-06 06:19:51 +0300

    update header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,13 +5,15 @@ import Navigation from './navigation';
 
 export default function Header() {
   return (
-    <header className="flex justify-center items-center container relative py-6 bg-almost-white">
-      <div className="absolute left-16">
-        <NavLink className="text-dark font-bold" to="/">
-          <img src="./logo.svg" alt="TravelTruks" />
-        </NavLink>
+    <header className="bg-almost-white">
+      <div className="flex justify-center items-center container relative py-6">
+        <div className="absolute left-16">
+          <NavLink className="text-dark font-bold" to="/">
+            <img src="./logo.svg" alt="TravelTruks" />
+          </NavLink>
+        </div>
+        <Navigation />
       </div>
-      <Navigation />
     </header>
   );
 }
